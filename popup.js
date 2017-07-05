60bde819e8a7762278369ed5794c70f199a10bdc
Justin Mao <justin.mao@mail.utoronto.ca>
2017-07-05 13:04:25 +0800

    Fixed flexbox favicons not displaying properly

--- popup.js
+++ popup.js
@@ -2,10 +2,10 @@ function storeSession() {
   chrome.windows.getCurrent(function(win) {
     chrome.tabs.getAllInWindow(win.id, function(tabs) {
       // Retrieve existing sessions from persistent storage.
-      chrome.storage.sync.get('sessions', function(items) {
+      chrome.storage.local.get('sessions', function(items) {
         sessions = items.sessions;
         // Initialize if no existing sessions are found.
-        if (items.sessions == null) {
+        if (sessions == null) {
           console.log("Initializing session storage.");
           sessions = [tabs];
         } else {
@@ -13,10 +13,10 @@ function storeSession() {
           sessions.push(tabs);
         }
         // Save the updated session list in persistent storage.
-        chrome.storage.sync.set({'sessions': sessions}, function() {
+        chrome.storage.local.set({'sessions': sessions}, function() {
           // Call view update function.
           console.log("Saved updated sessions.");
-          //update();
+          showSessions();
         });
       });
     });
@@ -24,27 +24,48 @@ function storeSession() {
 }
 
 function showSessions() {
-  chrome.storage.sync.get('sessions', function(items) {
+  var ignoredUrls = [
+    "chrome://extensions",
+    "chrome://history",
+    "chrome://settings",
+  ];
+  chrome.storage.local.get('sessions', function(items) {
     sessions = items.sessions;
     $('#sessions').empty();
-    // Bugs out if sessions is undefined.
+    // Pass an empty object if no sessions are found.
+    if (sessions == null) {
+      sessions = {};
+    }
     for (var i = 0; i < sessions.length; ++i) {
-      $('#sessions').append('<li>' + sessions[i] + '</li>');
+      var session = sessions[i];
+      var sessionString = '<div class="session">';
+      // Build append string.
+      for (var j = 0; j < session.length; ++j) {
+        var tab = session[j];
+        // Skip displaying icons of pages without favicons and pages with chrome theme favicons.
+        if (tab.favIconUrl != null) {
+          if (!tab.favIconUrl.includes("chrome://")) {
+            sessionString += '<img class="tab" src=' + tab.favIconUrl + '>'
+          }
+        }
+      }
+      $('#sessions').append(sessionString + '</div>');
     }
   });
 }
 
 function showSessionsConsole() {
-  chrome.storage.sync.get('sessions', function(items) {
+  chrome.storage.local.get('sessions', function(items) {
     sessions = items.sessions;
     console.log(sessions);
   });
 }
 
 function reset() {
-  chrome.storage.sync.remove('sessions', function() {
-    chrome.storage.sync.get('sessions', function(items) {
+  chrome.storage.local.remove('sessions', function() {
+    chrome.storage.local.get('sessions', function(items) {
       console.log("Reset!");
+      showSessions();
     });
   });
 }
@@ -55,3 +76,5 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("showSessionsConsole").addEventListener('click', showSessionsConsole);
   document.getElementById("clearSessions").addEventListener('click', reset);
 });
+
+document.onload = showSessions();
